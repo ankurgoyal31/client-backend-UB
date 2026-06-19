69cd0e839106a15a89f85974c086dabe60920793
ankurgoyal31 <ankurgoyal1227@gmail.com>
2026-06-20 00:55:57 +0530

    Update applyRoutes.js

--- routes/applyRoutes.js
+++ routes/applyRoutes.js
@@ -5,5 +5,6 @@ const { isAuthenticated } = require('../middleware/authMiddleware');
 const upload = require('../middleware/upload');
   
 router.get("/",isAuthenticated,applyController.list);
+router.delete("/delete/:id",isAuthenticated,applyController.delete);
 
 module.exports = router
